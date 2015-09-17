53114c5cf30009725997889cbf16d0026fd076e4
p0n1X <p0n1X@yahoo.com>
2015-09-17 17:27:32 +0300

    upload source

---
+++ Controller/SecurityController.php
@@ -0,0 +1,37 @@
+<?php
+namespace Custom\CMSBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login_route")
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+
+        return $this->render(
+            'CustomCMSBundle:Security:login.html.twig',
+            array(
+                'error'         => $error,
+            )
+        );
+    }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheckAction()
+    {
+        // this controller will not be executed,
+        // as the route is handled by the Security system
+    }
+}
